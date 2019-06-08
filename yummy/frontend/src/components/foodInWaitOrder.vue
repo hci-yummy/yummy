df4ddac5666dc2d7ec08850cc577eed076496e6a
Erica-WX <2544194336@qq.com>
2019-06-08 10:55:07 +0800

    完成待接单订单组件

---
+++ yummy/frontend/src/components/foodInWaitOrder.vue
@@ -0,0 +1,18 @@
+<template>
+  <div style="display:flex;width: 100%;margin-top: 10px">
+    <div style="width: 80%">{{food.name}}</div>
+    <div style="width: 11%">x{{food.num}}</div>
+    <div style="width: 9%">{{food.sum}}</div>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "food-in-wait-order",
+      props:["food"]
+    }
+</script>
+
+<style scoped>
+
+</style>
--- yummy/frontend/src/components/waitReceiveOrder.vue
+++ yummy/frontend/src/components/waitReceiveOrder.vue
@@ -1,13 +1,94 @@
 <template>
-    $END$
+    <div>
+      <el-card class="order">
+        <div slot="header" class="clearfix">
+          <span>{{order.time}}</span>
+          <el-button style="float: right; padding: 3px 0" type="text">接单&nbsp&nbsp&nbsp</el-button>
+        </div>
+        <div>
+          <div style="display: flex">
+            <div class="person">
+              <p>
+                <strong>{{order.name}}</strong>
+              </p>
+
+              <div>
+                {{order.address}}
+              </div>
+            </div>
+            <div class="tele">
+              <p style="color: #409EFF;"> <i class="el-icon-phone"></i>{{order.tele}}</p>
+            </div>
+          </div>
+          <el-divider></el-divider>
+        </div>
+
+
+        <div v-show="order.tip.length !== 0">
+          <div> <span style="color: #ff6445;">备注： </span>{{order.tip}}</div>
+          <el-divider></el-divider>
+        </div>
+
+        <div>
+          <div>
+            <strong>商品</strong>
+          </div>
+          <foodInWaitOrder :food="item" v-for="item in order.food" :key="item.name"></foodInWaitOrder>
+
+        </div>
+      </el-card>
+    </div>
 </template>
 
 <script>
+    import foodInWaitOrder from './foodInWaitOrder'
     export default {
-        name: "wait-receive-order"
+      name: "wait-receive-order",
+      components:{foodInWaitOrder},
+      data() {
+        return {
+          order: {
+            time: '2019/06/08 12:03:07',
+            name: '某翔',
+            tele: '18861524027',
+            address: '南京大学鼓楼校区',
+            tip: '不要辣，不要一次性筷子',
+            food: [
+              {
+              name: '脆骨丸子',
+              num: 3,
+              sum: 16.9
+            },
+              {
+                name: '照烧鸡排饭',
+                num: 1,
+                sum: 25.9
+              }]
+          }
+        }
+      }
+
     }
 </script>
 
 <style scoped>
+  .order{
+    width: 700px;
+    min-height: 200px;
+    /*padding-left: 10px;*/
+  }
 
+  .person{
+    /*border: 1px solid black;*/
+    width: 81%;
+    height: 70px;
+    /*margin-left: 10px;*/
+  }
+
+  .tele{
+    /*border: 1px solid black;*/
+    width: 19%;
+    height: 70px;
+    /*padding-left: 120px;*/
+  }
 </style>
