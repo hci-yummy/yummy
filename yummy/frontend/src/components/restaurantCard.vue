1dd31a5e5e1cfc7056900e04fe034e8fadd55d30
161250127 <361487214@qq.com>
2019-06-13 21:33:47 +0800

    地址选择器样式修改

--- yummy/frontend/src/components/restaurantCard.vue
+++ yummy/frontend/src/components/restaurantCard.vue
@@ -1,8 +1,8 @@
 <template>
-    <el-card style="width: 380px; margin-top: 20px; margin-right: 5px;">
+    <el-card style="width: 380px; margin-top: 20px; margin-right: 5px; height: 220px">
       <div style="display: flex;" @click="enter_rest">
         <img :src="info.url" style="width: 150px; height: 150px; border: 1px solid gainsboro;"/>
-        <div style="margin-left: 40px; font-size: 14px; color: grey">
+        <div style="margin-left: 20px; font-size: 14px; color: grey; max-width:180px ">
           <div style="font-size: 24px; font-weight: bold; margin-bottom: 10px; color: black; width: 200px; overflow: hidden;text-overflow: ellipsis"> <nobr>{{info.name}}</nobr></div>
           <el-tag style="margin-bottom: 10px" size="mini">{{info.type}}</el-tag>
           <el-rate
