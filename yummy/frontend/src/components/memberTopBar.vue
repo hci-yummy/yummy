48ceeb0a2b54fbea4e9be76cac63064754db9f96
161250127 <361487214@qq.com>
2019-06-13 09:15:42 +0800

    临时logout

--- yummy/frontend/src/components/memberTopBar.vue
+++ yummy/frontend/src/components/memberTopBar.vue
@@ -23,6 +23,7 @@
           if(this.activeIndex === '4'){
             //TODO 个人中心
             this.logout();
+            this.$router.push({name: "welcome"})
           }else if(this.activeIndex === '1'){
             this.$router.push({name: "welcome"})
           }else if(this.activeIndex === '3'){
