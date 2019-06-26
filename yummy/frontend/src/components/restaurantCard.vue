ef9d52263e579112d8b8364f419a388e0737b257
161250127 <361487214@qq.com>
2019-06-26 23:17:12 +0800

    附近的餐厅修改

--- yummy/frontend/src/components/restaurantCard.vue
+++ yummy/frontend/src/components/restaurantCard.vue
@@ -1,10 +1,10 @@
 <template>
-    <el-card style="width: 380px; margin-top: 20px; margin-right: 5px; height: 220px">
+    <el-card style="width: 285px; margin-top: 20px; margin-right: 3px; height: 180px">
       <div style="display: flex;" @click="enter_rest">
-        <img :src="info.url" style="width: 150px; height: 150px; border: 1px solid gainsboro;"/>
-        <div style="margin-left: 20px; font-size: 14px; color: grey; max-width:180px ">
-          <div style="font-size: 24px; font-weight: bold; margin-bottom: 10px; color: black; width: 200px; overflow: hidden;text-overflow: ellipsis"> <nobr>{{info.name}}</nobr></div>
-          <el-tag style="margin-bottom: 10px" size="mini">{{info.type}}</el-tag>
+        <img :src="info.url" style="min-width: 100px; max-width: 100px; height: 100px; border: 1px solid gainsboro;margin-top: 5px"/>
+        <div style="margin-left: 20px; font-size: 10px; color: grey; max-width:140px ">
+          <div style="font-size: 24px; font-weight: bold; margin-bottom: 5px; color: black; width: 200px; overflow: hidden;text-overflow: ellipsis"> <nobr>{{info.name}}</nobr></div>
+          <el-tag style="margin-bottom: 5px" size="mini">{{info.type}}</el-tag>
           <el-rate
             v-model="info.rate"
             disabled
@@ -12,7 +12,7 @@
             text-color="#ff9900"
             score-template="{value}">
           </el-rate>
-          <div style="font-size: 14px; margin-left: 2px">{{info.person}}人评价</div>
+          <div style="margin-left: 2px">{{info.person}}人评价</div>
           <div style="padding-top: 10px; margin-left: 2px">预计送达时间：{{info.time}}</div>
           <div style="padding-top: 10px; margin-left: 2px">配送费：{{info.price}}</div>
         </div>
