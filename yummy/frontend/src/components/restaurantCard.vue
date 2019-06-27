bb634afac9b403d4234df82ab398ac0a1a1ff904
161250127 <361487214@qq.com>
2019-06-28 00:04:37 +0800

    debug

--- yummy/frontend/src/components/restaurantCard.vue
+++ yummy/frontend/src/components/restaurantCard.vue
@@ -3,7 +3,7 @@
       <div style="display: flex;" @click="enter_rest">
         <img :src="info.url" style="min-width: 100px; max-width: 100px; height: 100px; border: 1px solid gainsboro;margin-top: 5px"/>
         <div style="margin-left: 12px; font-size: 10px; color: grey; max-width:140px ">
-          <div style="font-size: 24px; font-weight: bold; margin-bottom: 5px; color: black; width: 160px; overflow: hidden;text-overflow: ellipsis"> <nobr>{{info.name}}</nobr></div>
+          <div style="font-size: 24px; font-weight: bold; margin-bottom: 5px; color: black; width: 150px; overflow: hidden;text-overflow: ellipsis"> <nobr>{{info.name}}</nobr></div>
           <el-tag style="margin-bottom: 5px" size="mini">{{info.type}}</el-tag>
           <el-rate
             v-model="info.rate"
